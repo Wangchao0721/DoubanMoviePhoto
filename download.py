9ce097a5f835ac5d37a47b0f8637cd3102106c12
Chao Van <wangchao0721@gmail.com>
2011-12-08 11:53:49 +0800

    add pages

--- download.py
+++ download.py
@@ -8,25 +8,32 @@ from urllib import urlretrieve
 import os
 import sys
 
-url = r'http://movie.douban.com/subject/4920528/photos?type=S'
-out_folder = "/test/"
-def main(url):
-  soup = bs(urlopen(url))
-  parsed = list(urlparse.urlparse(url))
+starturl = r'http://movie.douban.com/subject/4920528/photos?type=S'
+def download(url):
+    soup = bs(urlopen(url))
+    parsed = list(urlparse.urlparse(url))
 
-  for image in soup.findAll("img"):
-    print "Image:%(src)s" % image
-    filename = image["src"].split("/")[-1]
-    parsed[2] = image["src"]
-    if parsed[2].find('thumb') != -1:
-      imageurl = parsed[2].replace('thumb','raw')
-    else:
-      imageurl = parsed[2]
-    print "ImagePath:%s" % parsed[2]
-    outpath = os.path.join(filename)
-    if image["src"].lower().startswith("http"):
-      urlretrieve(imageurl, outpath)
-    else:
-      urlretrieve(urlparse.urlunparse(parsed), outpath) 
+    for image in soup.findAll("img"):
+        print "Image:%(src)s" % image
+        filename = image["src"].split("/")[-1]
+        parsed[2] = image["src"]
+        if parsed[2].find('thumb') != -1:
+            imageurl = parsed[2].replace('thumb','raw')
+        else:
+            imageurl = parsed[2]
+        print "ImagePath:%s" % imageurl
+        outpath = os.path.join(filename)
+        #outpath = os.path.dirname(os.path.abspath(__file__))
+        print outpath
+        if image["src"].lower().startswith("http"):
+            urlretrieve(imageurl, outpath)
+        else:
+            urlretrieve(urlparse.urlunparse(parsed),outpath)
 
-main(url)
+    nexturl = soup.find(rel='next')['href']
+    print nexturl
+    if nexturl != None:
+        download(nexturl)
+
+download(starturl)
+
